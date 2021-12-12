10fe381d30f2354c637664aaac6cd8939fcdd003
kuritaeiji <eiji199825@yahoo.co.jp>
2021-12-12 12:50:07 +0900

    projects

---
+++ pages/projects.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1 class="grey--text font-weight-bold text-subtitle-1 mx-4">
+      {{ title }}
+    </h1>
+
+    <v-container>
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project of projects" :key="project.title">
+          <v-expansion-panel-header>
+            {{ project.title }}
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content class="grey--text">
+            <div class="font-weight-bold">
+              due by {{ project.due }}
+            </div>
+            <div>
+              {{ project.content }}
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'プロジェクト一覧',
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' }
+      ]
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
